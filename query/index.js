3fcab1c7e7617f47ddc3a19becdcf495c1a1c939
Bipul Mandol <mbbipulmandol.cse3.bu@gmail.com>
2020-08-19 18:25:44 +0600

    Add Moderation service

--- query/index.js
+++ query/index.js
@@ -21,13 +21,25 @@ app.post('/events', (req,res) => {
     }
 
     if(type === "CommentCreated"){
-        const { id,content,postId } = data;
+        const { id,content,postId,status } = data;
         
         const post = posts[postId] ;
-        post.comments.push({ id, content });
+        post.comments.push({ id, content , status });
 
     }
 
+    if(type === "CommentModerated"){
+        const { id,content,postId,status } = data;
+        
+        const post = posts[postId] ;
+        const comment = post.comments.find( c => {
+            return c.id === id;
+        });
+
+        comment.status = status;
+        comment.comment = content;
+        
+    }
     console.log(posts);
 
     res.send({});
