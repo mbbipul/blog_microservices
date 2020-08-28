c87e3bf18d68e3d5c8441bb81c2a8f1839d6c63e
Bipul Mandol <mbbipulmandol.cse3.bu@gmail.com>
2020-08-28 11:38:21 +0600

    Add all rest service k8s deployment and change url

--- event-bus/index.js
+++ event-bus/index.js
@@ -13,10 +13,10 @@ app.post('/events' , (req,res) => {
 
     events.push(event);
 
-    axios.post('http://localhost:4000/events',event);
-    axios.post('http://localhost:4001/events',event);
-    axios.post('http://localhost:4002/events',event);
-    axios.post('http://localhost:4003/events',event);
+    axios.post('http://posts-serv:4000/events',event);
+    axios.post('http://comments-serv:4001/events',event);
+    axios.post('http://query-serv:4002/events',event);
+    axios.post('http://moderation-serv:4003/events',event);
 
     res.send({ status : "ok"});
 
